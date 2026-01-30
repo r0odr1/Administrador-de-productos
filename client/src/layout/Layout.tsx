b83917ac4adf6d74fb8b01acf00214904f19ba74
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2026-01-30 14:19:37 -0500

    Trabajando en layout

--- client/src/layout/Layout.tsx
+++ client/src/layout/Layout.tsx
@@ -3,9 +3,17 @@ import { Outlet } from "react-router-dom";
 export default function Layout() {
   return (
     <>
-      <div>Desde Layout</div>
+      <header className="bg-slate-800">
+        <div className="mx-auto max-w-6xl py-10">
+          <h1 className="text-4xl font-extrabold text-white">
+            Administrador de Productos
+          </h1>
+        </div>
+      </header>
 
-      <Outlet />
+      <main className="mt-10 mx-auto max-w-6xl p-10 bg-white shadow rounded-lg">
+        <Outlet />
+      </main>
     </>
   )
 }
