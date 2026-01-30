0e1d528586e2fbbbc0f59e5057da2b14f4ad2ed4
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2026-01-30 11:43:59 -0500

    Agregando data router de react router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,11 @@
+import ReactDom from 'react-dom/client'
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+ReactDom.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
