bd230a7a83b033ce90e47a979e4a0b81f22abeb7
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2026-02-02 12:01:31 -0500

    Creando ruta para editar productos

--- client/src/components/ProductDetails.tsx
+++ client/src/components/ProductDetails.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom"
 import type { Product } from "../types"
 import { formatCurrency } from "../utils"
 
@@ -5,6 +6,8 @@ type ProductDetailsProps = {
   product: Product
 }
 export default function ProductDetails({ product } : ProductDetailsProps) {
+
+  const navigate = useNavigate()
 
   const isAvailable = product.availability
 
@@ -20,6 +23,12 @@ export default function ProductDetails({ product } : ProductDetailsProps) {
         {isAvailable ? 'Disponible' : 'No Disponible'}
       </td>
       <td className="p-3 text-lg text-gray-800 ">
+        <div className="flex gap-2 items-center">
+          <button
+            onClick={() => navigate(`/productos/${product.id}/editar`)}
+            className="bg-indigo-600 text-white rounded-lg w-full uppercase font-bold text-xs text-center"
+          >Editar</button>
+        </div>
       </td>
     </tr>
   )
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -4,6 +4,7 @@ import Products from "./views/Products";
 import { loader as productsLoader } from "./views/ProductsLoader";
 import NewProduct from "./views/NewProduct";
 import { action as newProductAction } from "./views/NewProductAction";
+import EditProduct from "./views/EditProduct";
 
 export const router = createBrowserRouter([
   {
@@ -19,6 +20,10 @@ export const router = createBrowserRouter([
         path: 'productos/nuevo',
         element: <NewProduct />,
         action: newProductAction
+      },
+      {
+        path: 'productos/:id/editar', //ROA Patter - Resource-oriented design
+        element: <EditProduct />
       }
     ]
   },
