54459bf48d71c6ea9796c96855a68e44ff02cafa
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2026-02-02 11:15:31 -0500

    Mostrando Productos

--- client/src/views/Products.tsx
+++ client/src/views/Products.tsx
@@ -1,15 +1,20 @@
 
-import { Link } from "react-router-dom"
+import { Link, useLoaderData } from "react-router-dom"
 import { getProducts } from "../services/ProductService";
+import ProductDetails from "../components/ProductDetails";
+import type { Product } from "../types";
 
 export async function loader() {
   const products = await getProducts()
-  console.log(products);
 
-  return {}
+  return products
 }
 
 export default function Products() {
+
+  const products = useLoaderData() as Product[]
+
+
   return (
     <>
       <div className="flex justify-between">
@@ -20,6 +25,29 @@ export default function Products() {
         >
           Agregar Producto
         </Link>
+      </div>
+
+      <div className="p-2">
+        <table className="w-full mt-5 table-auto">
+          <thead className="bg-slate-800 text-white">
+              <tr>
+                  <th className="p-2">Producto</th>
+                  <th className="p-2">Precio</th>
+                  <th className="p-2">Disponibilidad</th>
+                  <th className="p-2">Acciones</th>
+              </tr>
+          </thead>
+          <tbody>
+            {products.map(product => (
+              <ProductDetails
+                key={product.id}
+                product={product}
+
+              />
+            ))}
+
+          </tbody>
+        </table>
       </div>
 
     </>
